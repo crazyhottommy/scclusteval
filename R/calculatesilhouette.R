c7ed17bf781b64a3f1822641fad08dc60b2a5909
Ming Tang <tangming2005@gmail.com>
2019-09-18 11:13:33 -0400

    fix typo

--- R/calculatesilhouette.R
+++ R/calculatesilhouette.R
@@ -12,10 +12,11 @@
 #' CalculateSilhouette(pbmc_small, dims = 1:15)
 #'
 CalculateSilhouette<- function(object, dims = 1:50){
-        if (length(dims) > ncol(pbmc_small@reductions$pca@cell.embeddings)) {
+        if (length(dims) > ncol(object@reductions$pca@cell.embeddings)) {
                 stop("please specify PCA dims smaller than calculated")
         }
         cell_distance<- dist(object@reductions$pca@cell.embeddings[, dims])
+        # or as.integer
         cell_cluster<- as.numeric(as.character(Idents(object)))
         silhouette_score<- cluster::silhouette(cell_cluster, cell_distance)
         silhouette_score<- tibble::tibble(cluster = silhouette_score[,1], width = silhouette_score[,3]) %>%
